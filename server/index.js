2d7ccf0dc4a75c125f698337b13c1835a0ffa768
sauravshete2003 <sauravshete72@gmail.com>
2024-08-21 08:37:40 +0530

    basic setup done

---
+++ server/index.js
@@ -0,0 +1,23 @@
+import express from "express";
+import mongoose from "mongoose";
+import cors from "cors";
+import dotenv from "dotenv";
+dotenv.config();
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+const PORT = process.env.PORT;
+const MONGO_URL = process.env.MONGO_URL;
+
+(async () => {
+  const conn = await mongoose.connect(MONGO_URL);
+
+  if (conn) {
+    console.log("MongoDB connected ✅");
+  }
+})();
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
